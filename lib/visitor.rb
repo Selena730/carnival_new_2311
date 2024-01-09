62eb253e9342ea35055aebe31882b9ac49cb3f67
Selena Hawamdeh <selenahawamdeh@gmail.com>
2024-01-09 09:36:04 -0700

    Add add_preference method

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,12 +1,17 @@
 class Visitor
-  attr_reader :name, :height, :spending_money
+  attr_reader :name, :height, :spending_money, :preferences
 
   def initialize(name, height, spending_money)
     @name = name
     @height = height
-    @spending_money = spending_money
+    @spending_money = spending_money.delete_prefix('$').to_i
     @preferences = []
   end
+
+  def add_preference(pref)
+    @preferences << pref
+  end
+
 
 
 end
