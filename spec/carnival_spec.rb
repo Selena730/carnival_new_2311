61ebd8dfb69413dd03d07a3f2451ec1c0577fc2a
Selena Hawamdeh <selenahawamdeh@gmail.com>
2024-01-09 11:00:59 -0700

    Pass first test

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -1,1 +1,21 @@
 require 'spec_helper'
+require 'rspec'
+
+describe Carnival do
+  before :each do
+    @carnival = Carnival.new(14) # 14 days duration
+    @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+    @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Alice', 60, '$15')
+  end
+
+  it 'exists' do
+
+    expect(@carnival). to be_instance_of(Carnival)
+  end
+
+
+end
